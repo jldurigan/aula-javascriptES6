caa1286699cc8dce813b19cec4425618ea091d65
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2021-01-07 13:55:12 -0300

    tabela pronta

--- js/main.js
+++ js/main.js
@@ -34,3 +34,21 @@ var dados = [
         valor: 100
     },
 ]
+
+function tabela(){
+    let element = document.getElementById("corpo");
+    let html;
+    html="<table><tr><th>"+Object.keys(dados[0]).join("</th><th>")+"</th></tr>";
+    //(Object.keys(dados[0]);
+
+    for (let i=0; i<dados.length; i++){
+        html+="<tr>"
+        for(const prop in dados[i]){
+            html+="<td>"+dados[i].nome +"</td><td>"+dados[i].valor+"</td></tr>";
+            break;
+        }
+        html+="</tr>"
+    }
+    html+="</table>";
+    element.innerHTML=html;
+}
