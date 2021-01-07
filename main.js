e401e737e8eda9e1e50b0420cb60e202ffa3483e
jldurigan <lucasdurigan@hotmail.com>
2021-01-06 23:41:18 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,36 @@
+var dados = [
+    {
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },{
+        nome: 'carro',
+        valor: 100
+    },
+]
---
+++ teste.js
@@ -0,0 +1,11 @@
+var array=[1,2,3];
+let item;
+var teste=array.map(y =>{
+    item="x";
+    return {y};
+})
+console.log(item);
+console.log(teste);
+console.log(array);
+array.push(4);
+console.log(array);
