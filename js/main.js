ba2fc232ccbd1906ef518029a22d006f283d0d25
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2021-01-12 14:59:57 -0300

    alterado forma de construir a tabela usando map

--- js/main.js
+++ js/main.js
@@ -41,14 +41,19 @@ function tabela(){
     html="<table><tr><th>"+Object.keys(dados[0]).join("</th><th>")+"</th></tr>";
     //(Object.keys(dados[0]);
 
-    for (let i=0; i<dados.length; i++){
-        html+="<tr>"
-        for(const prop in dados[i]){
-            html+="<td>"+dados[i].nome +"</td><td>"+dados[i].valor+"</td></tr>";
-            break;
-        }
-        html+="</tr>"
-    }
+    dados.map(dado => html+="<tr><td>"+dado.nome+"</td><td>"+ dado.valor+"</td></tr>");
+    // html+="<tr><td>"+ `${nome}: ${valor}` + "</td><tr>"{
+
+    // });
+
+    // for (let i=0; i<dados.length; i++){
+    //     html+="<tr>"
+    //     for(const prop in dados[i]){
+    //         html+="<td>"+dados[i].nome +"</td><td>"+dados[i].valor+"</td></tr>";
+    //         break;
+    //     }
+    //     html+="</tr>"
+    // }
     html+="</table>";
     element.innerHTML=html;
 }
